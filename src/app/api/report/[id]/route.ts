a2648428ba7a399b20139933286e672e41180b52
harshiill <harshilkhandelwal28@gmail.com>
2025-07-10 12:18:36 +0530

    Refactor PUT function to retrieve ReporterId from search parameters instead of function parameters

--- src/app/api/report/[id]/route.ts
+++ src/app/api/report/[id]/route.ts
@@ -2,12 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConfig";
 import { ReportModel } from "@/models/report.models";
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest) {
     await dbConnect();
 
     try {
+        const {searchParams } = new URL(req.url);
+        const ReporterId = searchParams.get("id");
         const { status } = await req.json();
-        const updated = await ReportModel.findByIdAndUpdate(params.id, { status }, { new: true });
+        const updated = await ReportModel.findByIdAndUpdate(ReporterId, { status }, { new: true });
 
         if (!updated) {
             return NextResponse.json({ success: false, message: "Report not found" }, { status: 404 });
